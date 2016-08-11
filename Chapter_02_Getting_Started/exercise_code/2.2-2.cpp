d5706d0b8593911f1ce56c29c086b9852a4df168
QiangL-DUT <490308560@qq.com>
2016-08-11 11:59:28 +0800

    Update 2.2-2.cpp

--- Chapter_02_Getting_Started/exercise_code/2.2-2.cpp
+++ Chapter_02_Getting_Started/exercise_code/2.2-2.cpp
@@ -3,12 +3,12 @@
 #include <vector>
 using namespace std;
 
-void SelectSort(vector<int>& A){
+void SelectSort(vector<int>& A) {
 	int n = A.size();
-	for (int i = 0; i < n - 1; ++i){
+	for (int i = 0; i < n - 1; ++i) {
 		int index = i;
-		for (int j = i + 1; j < n; ++j){
-			if (A[index] > A[j]){
+		for (int j = i + 1; j < n; ++j) {
+			if (A[index] > A[j]) {
 				index = j;
 			}
 		}
@@ -18,17 +18,17 @@ void SelectSort(vector<int>& A){
 	}
 }
 
-void Print(const vector<int>& v){
+void Print(const vector<int>& v) {
 	for (auto a : v) cout << a << " ";
 	cout << endl;
 }
 
 const int N = 10;
 
-int main(){
+int main() {
 	vector<int> A(N, 0);
 	srand((int)time(NULL));
-	for (int i = 0; i < N; ++i){
+	for (int i = 0; i < N; ++i) {
 		A[i] = rand() % 100;
 	}
 	cout << "Before sorting: " << endl;
