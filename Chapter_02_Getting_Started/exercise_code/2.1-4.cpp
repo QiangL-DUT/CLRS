d6f66fc83a3485d7117c7f1e8e6e8fb957baf772
QiangL-DUT <490308560@qq.com>
2016-08-11 12:02:15 +0800

    Update 2.1-4.cpp

--- Chapter_02_Getting_Started/exercise_code/2.1-4.cpp
+++ Chapter_02_Getting_Started/exercise_code/2.1-4.cpp
@@ -3,11 +3,11 @@
 #include <vector>
 using namespace std;
 
-vector<int> AddBinary(vector<int>& A, vector<int>& B){
+vector<int> AddBinary(vector<int>& A, vector<int>& B) {
 	int n = A.size();
 	vector<int> C(n + 1, 0);
 	int carry = 0;
-	for (int i = 0; i < n; ++i){
+	for (int i = 0; i < n; ++i) {
 		C[i] = (A[i] + B[i] + carry) % 2;
 		carry = (A[i] + B[i] + carry) / 2;
 	}
@@ -15,14 +15,14 @@ vector<int> AddBinary(vector<int>& A, vector<int>& B){
 	return C;
 }
 
-void PrintBinary(const vector<int>& v){
+void PrintBinary(const vector<int>& v) {
 	for (auto i : v) cout << i << " ";
 	cout << endl;
 }
 
 const int N = 5;
 
-int main(){
+int main() {
 	vector<int> A(N, 0);
 	vector<int> B(N, 0);
 	srand((int)time(NULL));
