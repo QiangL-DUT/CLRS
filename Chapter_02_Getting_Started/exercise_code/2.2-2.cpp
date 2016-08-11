cbc63dbe199737428cb4fdb37aafd426878d9251
QiangL-DUT <490308560@qq.com>
2016-08-11 11:57:01 +0800

    Create 2.2-2.cpp

---
+++ Chapter_02_Getting_Started/exercise_code/2.2-2.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <time.h>
+#include <vector>
+using namespace std;
+
+void SelectSort(vector<int>& A){
+	int n = A.size();
+	for (int i = 0; i < n - 1; ++i){
+		int index = i;
+		for (int j = i + 1; j < n; ++j){
+			if (A[index] > A[j]){
+				index = j;
+			}
+		}
+		int temp = A[index];
+		A[index] = A[i];
+		A[i] = temp;
+	}
+}
+
+void Print(const vector<int>& v){
+	for (auto a : v) cout << a << " ";
+	cout << endl;
+}
+
+const int N = 10;
+
+int main(){
+	vector<int> A(N, 0);
+	srand((int)time(NULL));
+	for (int i = 0; i < N; ++i){
+		A[i] = rand() % 100;
+	}
+	cout << "Before sorting: " << endl;
+	Print(A);
+	SelectSort(A);
+	cout << "After sorting: " << endl;
+	Print(A);
+	return 0;
+}
+
