e322203f8fa9e6994fe0fbecefe6879ac7d63b9b
QiangL-DUT <490308560@qq.com>
2016-08-17 14:40:34 +0800

    Create Queue.cpp

---
+++ Chapter_10_Elementary_Data_Structures/exercise_code/Queue.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+typedef struct Queue {
+	vector<int> v;
+	int head;
+	int tail;
+	int length;
+} Queue;
+
+void Init(Queue& Q, int size) {
+	Q.v.resize(size);
+	Q.head = 0;
+	Q.tail = 0;
+	Q.length = size;
+}
+
+void EnQueue(Queue& Q, int x) {
+	if ((Q.tail + 1) % Q.length == Q.head) {
+		cout << "underflow!" << endl;
+		return;
+		//exit(0);
+	}
+	Q.v[Q.tail] = x;
+	if (Q.tail + 1 == Q.length) {
+		Q.tail = 0;
+	}
+	else {
+		++Q.tail;
+	}
+}
+
+int DeQueue(Queue& Q) {
+	if (Q.tail == Q.head) {
+		cout << "overflow!" << endl;
+		return 0;
+		//exit(0);
+	}
+	int x = Q.v[Q.head];
+	if (Q.head == Q.length - 1) {
+		Q.head = 0;
+	}
+	else {
+		++Q.head;
+	}
+	return x;
+}
