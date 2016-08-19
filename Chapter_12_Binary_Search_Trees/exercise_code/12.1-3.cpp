cbab4fa9f4b7e63c87803335345038616e4c8e0e
QiangL-DUT <490308560@qq.com>
2016-08-19 22:11:27 +0800

    Create 12.1-3.cpp

---
+++ Chapter_12_Binary_Search_Trees/exercise_code/12.1-3.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+typedef struct Node {
+	int val;
+	Node* left;
+	Node* right;
+	Node(int val_ = 0, Node* left_ = NULL, Node* right_ = NULL) : 
+		val(val_), left(left_), right(right_) {}
+}Node;
+
+Node* CreateTree(const vector<int> v, int& i) {
+	++i;
+	if (i >= v.size()) {
+		return NULL;
+	}
+	if (v[i] == -1) {
+		return NULL;
+	}
+	Node* node = new Node(v[i]);
+	node->left = CreateTree(v, i);
+	node->right = CreateTree(v, i);
+	return node;
+}
+
+void Destroy(Node* root) {
+	if (root) {
+		if (root->left) {
+			Destroy(root->left);
+		}
+		if (root->right) {
+			Destroy(root->right);
+		}
+		delete root;
+	}
+}
+
+void InOrder(Node* root) {
+	stack<Node*> S;
+	while (true) {
+		while (root) {
+			S.push(root);
+			root = root->left;
+		}
+		if (S.empty()) {
+			return;
+		}
+		root = S.top();
+		S.pop();
+		cout << root->val << " ";
+		root = root->right;
+	}
+}
+
+int main() {
+	vector<int> v = { 6, 5, 2, -1, -1, 5, -1, -1, 7, -1, 8, -1, -1 };  //Fig.12-1(a)
+	int i = -1;
+	Node* root = CreateTree(v, i);
+	InOrder(root);
+	Destroy(root);
+	return 0;
+}
